c604c6ddbe56e55e4abe16847a8bdec6772fd14d
Varun Singh <vs95764@gmail.com>
2024-11-15 01:01:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-// src/server.js
+require('dotenv').config();  
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -6,19 +7,17 @@ const contactRoutes = require('./routers/contactRoutes');
 const db = require('./db');
 
 const app = express();
-const port = 5000;
 
-// Middlewares
-app.use(cors());
+const port = process.env.PORT || 5000;  
+const corsOrigin = process.env.CORS_ORIGIN || '*';  
+
+app.use(cors({ origin: corsOrigin }));  
 app.use(bodyParser.json());
 
-// Connect to MongoDB
 db.connect();
 
-// Routes
 app.use('/contacts', contactRoutes);
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
