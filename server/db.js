7e84ce8938e6bf7920ec9294809246b8cdc0c550
Varun Singh <vs95764@gmail.com>
2024-11-15 01:29:27 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => {
-  console.error('MongoDB connection error:', err);
-  process.exit(1);
-});
+const connect = () => {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
+};
+
+module.exports = { connect };
